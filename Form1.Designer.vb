bb1c8bb02ffdb8d368b001472f790bf0cc1eb89f
Alimohammad Eghbaldar <ali.eghbaldar2@gmail.com>
2023-06-16 23:37:41 +0330

    first and last

---
+++ Form1.Designer.vb
@@ -0,0 +1,60 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Location = New System.Drawing.Point(43, 183)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(1012, 58)
+        Me.ProgressBar.TabIndex = 0
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(43, 95)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(1012, 58)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(17.0!, 41.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1080, 315)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ProgressBar As ProgressBar
+    Friend WithEvents btnStart As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Dim opf As New OpenFileDialog
+        opf.ShowDialog()
+        Dim f As New FileInfo(opf.FileName)
+        ProgressBar.Maximum = f.Length
+
+        Dim IP As String = "Your IP - or - Your Website's Name"
+        Dim USER As String = "FTP Username"
+        Dim PASS As String = "FTP Passwprd"
+
+        Dim request As FtpWebRequest = WebRequest.Create("ftp://" & IP & "/" & Guid.NewGuid.ToString & f.Extension)
+        request.Credentials = New NetworkCredential(USER, PASS)
+        request.Method = WebRequestMethods.Ftp.UploadFile
+
+        Using fileStream As Stream = File.OpenRead(opf.FileName),
+              ftpStream As Stream = request.GetRequestStream()
+            Dim read As Integer
+            Do
+                Dim buffer() As Byte = New Byte(10240) {}
+                read = fileStream.Read(buffer, 0, buffer.Length)
+                If read > 0 Then
+                    ftpStream.Write(buffer, 0, read)
+                    ProgressBar.Value = fileStream.Position '
+                End If
+            Loop While read > 0
+        End Using
+        ProgressBar.Value = 0
+
+
+    End Sub
+End Class
